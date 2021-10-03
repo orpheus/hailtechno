bf122ce5dde3d4084d7a318ea939fa7a7f7ce6aa
ohryan <ryanachacon@gmail.com>
2021-10-02 21:33:40 -0600

    new lein app project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hailtechno
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hailtechno/core_test.clj
@@ -0,0 +1,7 @@
+(ns hailtechno.core-test
+  (:require [clojure.test :refer :all]
+            [hailtechno.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
