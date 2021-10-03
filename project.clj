f9c42c8f082f7d5bf1d478c58ad2d447cf7c71d0
ohryan <ryanachacon@gmail.com>
2021-10-02 22:29:48 -0600

    basic web server with hot reload env

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.8.2"]
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [ring/ring-devel "1.8.2"]
+                 [compojure "1.6.2"]]
+  :ring {:handler hailtechno.core/app}
+  :plugins [[lein-ring "0.12.5"]]
   :main ^:skip-aot hailtechno.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/hailtechno/core.clj
+++ src/hailtechno/core.clj
@@ -1,7 +1,17 @@
 (ns hailtechno.core
-  (:gen-class))
+  (:gen-class)
+  (:use compojure.core)
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]
+            ))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defroutes main-routes
+  (GET "/" [] (str "What's up stunna"))
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+
+(def app
+  (-> (handler/site main-routes)))
