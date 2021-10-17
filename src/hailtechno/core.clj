eca1df59ba1a305b06839964c30b4e3f523aef1b
ohryan <ryanachacon@gmail.com>
2021-10-17 11:26:42 -0600

    beginning of file-upload-route wrapper

--- src/hailtechno/core.clj
+++ src/hailtechno/core.clj
@@ -91,9 +91,80 @@ CREATE TABLE IF NOT EXISTS tracks(
                (bad-request res)))
          )))
 
+(defn is-valid-req
+  "Returns null if valid. Returns error message if not."
+  [params config])
+
+(defn store-files [params config]
+  {:filepath "some/file/path"})
+
+(defn agg-metadata [params filepaths]
+  {})
+
+(defn file-upload-route
+  "Creates a configurable file upload route with validation.
+  Returns a composure route wrapped in ring's multipart middleware.
+
+  controller: String
+  config: Map {
+    :accepts Vector::String (content-type) [\"audio/mpeg\" \"audio/vnd.wav\"]
+    :filepath String \"path/to/save/file\"
+    :metadata Vector::String [\"trackname\" \"artistname\"]
+  }
+  callback: Fn (fn [Map {:keys [trackname artistname filepath]}]) ()
+
+  The `file-upload-route` looks for the `file` key to exist as part of the req
+  params. This `file` must have a `content-type` that matches one of
+  the values inside the `:accepts` vector
+
+  The `:filepath` is the location where the file will be stored.
+  Accepts templates such as 'file/{trackname}' where `{trackname}` will
+  be substituted for the `trackname` parameter as defined to lookup by the metadata.
+  Can also optionally template via `file/{trackname}/{album?}` where if `album`
+  is defined in the metadata, it will use it as part of the path.
+
+  The `:metadata` is a list of values to look up in the `params` map of
+  the request body to be used as templates in the filepath and as arguments
+  for the callback. The `file-upload-route` will only look at the variables
+  in the params map that are defined in the `:metadata` field. To mark
+  a field as requied, prefix it with an underscore. This will not change
+  how thpe callback fn will receive this parameter, e.g. the underscore will
+  be stripped after it has verified that value is non-null and non-empty.
+
+  The  `callback` parameter is a function that takes a map of metadata from
+  the request and vars calculated during execution for the user to use to
+  perform side effects like update a databas. It takes as a second argument
+  the actual http `request`.
+  "
+  [controller config callback]
+  (wrap-multipart-params
+   (POST controller request
+         (println request)
+         ;; Validate request body - returns null or error message
+         (let [error (is-valid-req (:params request) config)]
+           (if-not (nil? error)
+             (bad-request error)
+             ;; If no error, store file
+             (let [[filepaths error] (store-files (:params request) config)]
+               (if-not (nil? error)
+                 (bad-request error)
+                 ;; If no error, call callback with metadata
+                 (callback (agg-metadata (:params request) filepaths) request)))))
+         (response "OK"))))
+
+(defn track-route []
+  (file-upload-route
+   "/track"
+   {:accepts ["audio/mpeg" "audio/vnd.wav" "audio/mp4"]
+    :filepath "track/{artist}/{album?}"
+    :metadata ["_name" "_artist" "album"]}
+   (fn [{:keys [name artist album filepath]}]
+     (println name artist album filepath))))
+
 (defroutes main-routes
   (GET "/" [] (str "What's up stunna"))
-  (upload-route))
+  (upload-route)
+  (track-route))
 
 
 (def app
